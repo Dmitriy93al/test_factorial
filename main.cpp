f7f530fbfb834421a3c5835ca54c855e9bab7912
Dmitiy Alekseev <dmitriy.dog93@mail.ru>
2024-05-11 21:48:41 +0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,33 @@
+
+#include <catch2/catch_test_macros.hpp>
+
+#include <cstdint>
+
+#include <iostream>
+
+#include<catch2/catch_session.hpp>
+#include"list.cpp"
+
+
+
+unsigned long m_size;
+
+bool Empty() { return m_size == 0; }
+
+TEST_CASE("Empty are computed", "[Empty]")
+{
+    REQUIRE(Empty() == 1);
+
+}
+
+
+unsigned long Size() { return m_size; }
+
+TEST_CASE("Size are computed", "[Size]")
+{
+    REQUIRE(Size() == 1);
+    REQUIRE(Size() == 2);
+    REQUIRE(Size() == 3);
+    REQUIRE(Size() == 4);
+    REQUIRE(Size() == 5);
+}
